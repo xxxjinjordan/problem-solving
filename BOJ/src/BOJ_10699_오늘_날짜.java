deba66cf2bfd6ab096995c887f196753e4f804a7
JDEV <xxxjinjordan@gmail.com>
2022-09-06 23:58:11 +0900

    220906 problem upload

---
+++ BOJ/src/BOJ_10699_오늘_날짜.java
@@ -0,0 +1,10 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class BOJ_10699_오늘_날짜 {
+    public static void main(String[] args) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date d = new Date();
+        System.out.println(format.format(d));
+    }
+}
